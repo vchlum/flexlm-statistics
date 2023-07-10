15b84ba1d4ff14d48ff6edc30d738cd6918cdaa1
Václav Chlumský <chlumskyvaclav@gmail.com>
2023-07-10 12:39:25 +0200

    proper respect of restarts and shutdowns

--- flexlm-statistics.sh
+++ flexlm-statistics.sh
@@ -2,15 +2,21 @@
 
 # usage example: ./flexlm-statistics.sh flexlm_server1.log flexlm_server2.log  flexlm_lm-server3.log
 
+INIT_DATE="3/19/2020"
 MIN_TS=1640995200
 LICENSE="\"MATLAB\""
 USERS=(
 "vchlum"
 )
+
 HOSTNAMES=".+cz"
+
+#USERS=".*"
+#HOSTNAMES=".*"
 
 TS_ALL="ts_all"
 TS_ALL_SORTED="${TS_ALL}_sorted"
+TS_RESETS_ADDED="${TS_ALL}_resets"
 TS_ALL_SORTED_USERS="${TS_ALL}_users"
 TS_ALL_SORTED_LICENSE="${TS_ALL_SORTED_USERS}_license"
 FINAL="final.dat"
@@ -19,8 +25,8 @@ TS_FILES=()
 for FILE in "$@"
 do
     TS_FILE="ts_$FILE"
-    cat $FILE | grep -E "^[0-9]+:[0-9]+:[0-9]+ .+" | awk '
-        BEGIN{ts=0; last_ts=0;}
+    cat $FILE | grep -E "^[0-9]+:[0-9]+:[0-9]+ .+" | awk -v init_date=$INIT_DATE '
+        BEGIN{ts=0; last_ts=0; split(init_date, d, "/")}
         {
 			split($1, t, ":")
 
@@ -55,6 +61,12 @@ done
 
 cat $TS_ALL | sort -s -n -k 1,1 > $TS_ALL_SORTED
 
+cat $TS_ALL_SORTED | awk 'BEGIN{master=""} {
+	if ($0 ~ /.*REStarted MLM.*/) print $1 " RESET_OUT";
+
+	print
+}' > $TS_RESETS_ADDED
+
 ALL_USERS=""
 for USER in "${USERS[@]}" ; do
 	ALL_USERS="${USER}|$ALL_USERS"
@@ -63,26 +75,26 @@ ALL_USERS=${ALL_USERS::-1}
 
 USERS_REGEXP="^($ALL_USERS)@$HOSTNAMES"
 
-cat $TS_ALL_SORTED | awk -v r=$USERS_REGEXP '{if ($6 ~ r) print}' > $TS_ALL_SORTED_USERS
+cat $TS_RESETS_ADDED | awk -v r=$USERS_REGEXP '{if ($6 ~ r) print; if ($2 == "RESET_OUT") print}' > $TS_ALL_SORTED_USERS
 
-cat $TS_ALL_SORTED_USERS | awk  -v lic=$LICENSE '{if ($5 == lic && ($4 == "IN:" || $4 == "OUT:")) print $1 " " $4 " " $6 " " $5}' > $TS_ALL_SORTED_LICENSE
+cat $TS_ALL_SORTED_USERS | awk  -v lic=$LICENSE '{if ($5 == lic && ($4 == "IN:" || $4 == "OUT:")) print $1 " " $4 " " $6 " " $5 " "$7; if ($2 == "RESET_OUT") print}' > $TS_ALL_SORTED_LICENSE
 
 cat $TS_ALL_SORTED_LICENSE | awk -v mts=$MIN_TS 'BEGIN{out=0; last_check_ts=0}
 {
 	node=$3
 	if ($2 == "IN:") {
-		if (node in lic)
-			{
-				lic[node]--
-				if (lic[node] == 0) {
-					out--
-				}
-			}
-		else
-			{
-				lic[node]=0
+		if ($5 == "(SHUTDOWN)") {
+			lic[node]=0
+			out--
+		} else if (node in lic) {
+			lic[node]--
+			if (lic[node] == 0) {
 				out--
 			}
+		} else {
+			lic[node]=0
+			out--
+		}
 	}
 	if ($2 == "OUT:") {
 		if (node in lic)
@@ -97,15 +109,9 @@ cat $TS_ALL_SORTED_LICENSE | awk -v mts=$MIN_TS 'BEGIN{out=0; last_check_ts=0}
 		last[node]=$1
 	}
 
-	if ($1 > last_check_ts) {
-		for (node in last) {
-			if (lic[node]> 0 && ($1 > last[node] + 2592000)) { # 30 days
-				lic[node]=0
-				out--
-				delete last[node]
-			}
-		}
-		last_check_ts=$1
+	if ($2 == "RESET_OUT") {
+		split("", lic)
+		out = 0
 	}
 
 	# licenses out
@@ -149,8 +155,6 @@ cat $FINAL | awk -v mts=$MIN_TS 'BEGIN {
 {
 	if (day == $1 && max < $4) {
 		max=$4
-		if (max > 450)
-			max=450
 	}
 
 	if (day != $1) {
@@ -167,7 +171,7 @@ cat $FINAL | awk -v mts=$MIN_TS 'BEGIN {
 			split(tmp_split[1], last_d, "/")
 
 			while ((curr_d[1] != last_d[1]) || (curr_d[2] != last_d[2]) || (curr_d[3] != last_d[3])) {
-				print last_d[1]"/"last_d[2]"/"last_d[3]" 0"
+				print last_d[1]"/"last_d[2]"/"last_d[3]" "max
 				
 				ts+=86400
 				tmp=strftime("%m/%d/%Y %H:%M:%S", ts)
